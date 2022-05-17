d66af464c5ef91d05625b918bf7b13035c96c9c8
Mark Worden <mprepelitsa05@mail.ru>
2022-05-17 13:00:00 +0300

    2 task

--- mentor_netology.py
+++ mentor_netology.py
@@ -6,13 +6,37 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lecturers(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            print("GOOD!")
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
+class Lecturer(Mentor):
+
+    def __init__(self, name, surname):
+        "Инициализирует атрибуты класса-родителя."
+        super().__init__(name, surname)
+        self.grades = {}
+        self.courses_attached = []
+
+class Reviewer(Mentor):
+
+    def __init__(self, name, surname):
+        "Инициализирует атрибуты класса-родителя."
+        super().__init__(name, surname)
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,18 +46,9 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-
-    def __init__(self, name, surname):
-        "Инициализирует атрибуты класса-родителя."
-        super().__init__(name, surname)
-
-class Reviewer(Mentor):
-
-    def __init__(self, name, surname):
-        "Инициализирует атрибуты класса-родителя."
-        super().__init__(name, surname)
-
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_lecturer = Lecturer('Mark', 'Worden')
+best_student.rate_lecturers(best_lecturer, 'Python', 10)
+print(best_lecturer.grades)
